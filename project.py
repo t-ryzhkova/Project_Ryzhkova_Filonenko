a4703304637e9024a3b640971636bf7c851dc378
dariiafilonenko <dariia.filonenko.25@kse.org.ua>
2026-07-19 20:17:04 +0300

    add adidas's stock

--- project.py
+++ project.py
@@ -1,6 +1,16 @@
 import yfinance as yf
+import pandas as pd
+import numpy as np
+import csv
+import json
 import matplotlib.pyplot as plt
 
-stockData = yf.download('PBYI','2022-07-01','2023-07-01')
+stockData = yf.download('ADS.DE','2026-01-01','2026-07-19')
 print(stockData)
 
+stockData.to_csv("Adidas_stock.csv", encoding="utf-8")
+
+
+df = pd.DataFrame(stockData)
+print("--- DataFrame ---")
+print(df)
